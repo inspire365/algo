6d7ebaa58dd61fae8dc9d45d43778e6fbe2d6d13
inspire365 <365days.enjoy@gmail.com>
2014-11-29 01:16:13 +0800

    insertion sort & findnth

--- common/sort.h
+++ common/sort.h
@@ -1,22 +1,19 @@
 #ifndef COMMON_CAN_SORT_H_
 #define COMMON_CAN_SORT_H_
 
+#include<assert.h>
+#include<iostream>
+
 namespace can { namespace common {
 
 template<typename T>
-int hoare_partition(T* base, int left, int right, int (*cmp)(const T&, const T&))
+int partition(T* base, const int left, const int right, int (*cmp)(const T&, const T&))
 {
-  // TODO
-}
-
-template<typename T>
-int partition(T* base, int left, int right, int (*cmp)(const T&, const T&))
-{
-  T pivot = base[right];
+  assert(base && cmp);
   int low = left - 1;
   for(int high = left; high < right; ++high)
   {
-    if(cmp(base[high], pivot) <= 0)
+    if(cmp(base[high], base[right]) < 0)
     {
       ++low;
       // swap
@@ -25,11 +22,46 @@ int partition(T* base, int left, int right, int (*cmp)(const T&, const T&))
       base[high] = tmp;
     }
   }
+  // the following swap make it unstable
   T tmp = base[low + 1];
   base[low + 1] = base[right];
   base[right]   = tmp;
   return (low + 1);
 }
+
+/**
+  find the nth element of the array
+  time complexity: O(lgn)
+**/
+template<typename T>
+T* findnth(
+    T*        base,
+    const int left,       // left most element
+    const int right,      // right most element
+    const int nth,        // start from 0
+    int (*cmp)(const T&, const T&))
+{
+  assert(base && cmp && nth <= (right - left));
+  int lhs = left;
+  int rhs = right;
+  int pivot = left;
+  do
+  {
+    pivot = partition(base, lhs, rhs, cmp);
+    int pos = pivot - left;
+    if(pos == nth)
+      break;
+    if(pivot > nth)
+    {
+      rhs = pivot - 1;
+    }
+    else
+    {
+      lhs = pivot + 1;
+    }
+  } while(true);
+  return (base + pivot);
+} // findnth
 
 /**
   Avery efficient with "divide and conquer" unstable sort algorithm
@@ -44,7 +76,29 @@ void qsort(T* base, int left, int right, int (*cmp)(const T&,const T&))
   int pivot = partition(base, left, right, cmp);
   qsort(base, left, pivot - 1, cmp);
   qsort(base, pivot + 1, right, cmp);
-}
+} // qsort
+
+/**
+  stable sort algorithm
+  time complexity: O(n^2)
+**/
+template<typename T>
+void insertion_sort(T* base, int len, int (*cmp)(const T& lhs, const T& rhs))
+{
+  assert(base && len > 0);
+  int forward = 1;
+  for(; forward < len; ++forward)
+  {
+    T key = base[forward];
+    int backward = forward - 1;
+    while(backward >= 0 && (cmp(base[backward], key) > 0))
+    {
+      base[backward + 1] =  base[backward];
+      --backward;
+    }
+    base[backward + 1] = key;
+  }
+} // insertion sort
 
 }}  // ::can::common
 
--- examples/main_sort.cpp
+++ examples/main_sort.cpp
@@ -21,7 +21,7 @@ int compare(const int& lhs, const int& rhs)
 
 int main(int argc, char** argv)
 {
-  int sample[] = {2, 8, 7, 1, 3, 5, 6, 4};
+  int sample[] = {2, 2, 4, 8, 5, 7, 1, 3, 5, 6, 4};
   cout << "sample values: " << endl;
   int size = sizeof(sample)/sizeof(int);
   cout << "sample size: " << size << "   values: " << endl;
@@ -31,7 +31,15 @@ int main(int argc, char** argv)
   }
   cout << "------------------------------" << endl;
 
-  ::can::common::qsort(sample, 0, size -1, compare);
+  // quick sort
+//  ::can::common::qsort(sample, 0, size -1, compare);
+  int pos = 10;
+  int* val = ::can::common::findnth(sample, 0, 10, pos, compare);
+  cout << pos << " th:" << *val << endl << endl;
+
+  // insertion sort
+  ::can::common::insertion_sort(sample, size, compare);
+
 
   for(int i = 0; i < size; ++i)
   {
